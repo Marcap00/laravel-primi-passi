b951acc7feb656675a247501a53a3488fe1ec44d
Marcap00 <cappellatomarco@yahoo.com>
2024-10-21 16:20:40 +0200

    started projects and create home page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <!-- Bootstrap 5.3.3 -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center">Homepage</h1>
+    </header>
+    <main>
+        <h2 class="text-center">La mia prima Homepage con Laravel</h2>
+    </main>
+</body>
+
+</html>
