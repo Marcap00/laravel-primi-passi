49eaba83d727f2004b6d2b7ad7c55b55f1abe567
Marcap00 <cappellatomarco@yahoo.com>
2024-10-21 16:34:37 +0200

    laravel homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Homepage</title>
     <!-- Bootstrap 5.3.3 -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
         integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
